c3a08034533067a628cb4b67b28b4385212081ae
consatanos <constantoskin@gmail.com>
2020-03-31 13:55:20 +0300

    add template load-more btn

--- src/main.js
+++ src/main.js
@@ -131,3 +131,13 @@ const createSiteSortTemplate = () => {
     </div>`
   );
 };
+
+/**
+ * Create template load-more btn
+ * @return {string} html template load-more btn
+ */
+const createSiteLoadMoreBtnTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
