fdd7caa791d96e6efde9e91cd03d57ca7897171b
consatanos <constantoskin@gmail.com>
2020-04-03 17:45:58 +0300

    breaking up file on modules

---
+++ src/components/load-more-btn-template.js
@@ -0,0 +1,13 @@
+/**
+ * Create template load-more btn
+ * @return {string} html template load-more btn
+ */
+const loadMoreBtnTemplate = () => {
+  return (
+    `<button class="load-more" type="button">load more</button>`
+  );
+};
+
+export {
+  loadMoreBtnTemplate
+};
