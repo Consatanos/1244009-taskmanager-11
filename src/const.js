377c56f87480815a41f3f7521b574359518a3f97
consatanos <constantoskin@gmail.com>
2020-04-11 11:13:09 +0300

    add const

---
+++ src/const.js
@@ -0,0 +1,34 @@
+/**
+ * Array colors
+ */
+const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
+
+/**
+ * Array days of week
+ */
+const DAYS = [`mo`, `tu`, `we`, `th`, `fr`, `sa`, `su`];
+
+/**
+ * Array month of year
+ */
+const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+
+export {
+  COLORS,
+  DAYS,
+  MONTH_NAMES
+};
