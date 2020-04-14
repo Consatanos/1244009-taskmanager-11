f56ec2cc189f57e9cbab72eb0d6f3b8296011469
consatanos <constantoskin@gmail.com>
2020-04-14 23:24:21 +0300

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,12 @@
 /**
+ * Render include position
+ */
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+};
+
+/**
  * Format time value from `1` to `01`
  * @param {*} value
  * @return {string}
@@ -17,4 +25,33 @@ export const formatTime = (date) => {
   const minutes = castTimeFormat(date.getMinutes());
 
   return `${hours}:${minutes}`;
+};
+
+/**
+ * Create DOM element
+ * @param {string} template
+ * @return {string}
+ */
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+/**
+ * Render DOM element
+ * @param {*} container
+ * @param {string} element
+ * @param {*} place
+ */
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
 };
